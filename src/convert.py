687d0d644a1a2442fdaa6aef497bc56f92601b87
--get <67471674+ninebolt6@users.noreply.github.com>
2023-07-30 21:51:52 +0900

    距離・角度情報を出力する

--- src/convert.py
+++ src/convert.py
@@ -3,14 +3,16 @@ from datetime import datetime
 from itertools import chain
 import json
 import os
+from typing import cast
 
 from natsort import natsorted
+from calc import length
 from dataclass import Person
 from keypoint import KeypointEnum
 
 from track import OUTPUT_FOLDER
-from usecase import warp_keypoints
-from util import as_person
+from usecase import Midpoint, get_body_orientation, warp_keypoints
+from util import as_person, sliding_window
 
 
 TARGET_FOLDER = "20230724_234531"
@@ -19,8 +21,16 @@ KEYPOINT_JSON_PATH = os.path.join(OUTPUT_FOLDER, TARGET_FOLDER, "keypoints")
 EXEC_TIME = f"{datetime.now().strftime('%Y%m%d_%H%M%S')}"
 
 
+def get_frame_num(filename: str) -> str:
+    return filename.replace("frame_", "").replace(".json", "")
+
+
 def create_position_header(id: int) -> list[str]:
     return [f"id:{id} x", f"id:{id} y"]
+
+
+def create_distance_degree_header(id: int) -> list[str]:
+    return [f"id:{id} dist", f"id:{id} deg"]
 
 
 def convert():
@@ -37,7 +47,10 @@ def convert():
 
     with open(
         os.path.join(CSV_OUTPUT_FOLDER, f"out_position_{EXEC_TIME}.csv"), "w", encoding="utf-8", newline=""
-    ) as position_out:
+    ) as position_out, open(
+        os.path.join(CSV_OUTPUT_FOLDER, f"out_distance_degree_{EXEC_TIME}.csv"), "w", encoding="utf-8", newline=""
+    ) as distance_degree_out:
+        # 位置座標
         position_header = ["frame_num"]
         position_header.extend(
             # flatten
@@ -46,26 +59,71 @@ def convert():
         position_writer = csv.DictWriter(position_out, fieldnames=position_header)
         position_writer.writeheader()
 
-        for filename in files:
-            with open(os.path.join(KEYPOINT_JSON_PATH, filename)) as f1:
-                current_list: list[Person] = json.load(f1, object_hook=as_person)
+        # 距離・角度
+        distance_degree_header = ["frame_num"]
+        distance_degree_header.extend(
+            # flatten
+            chain.from_iterable(map(lambda id: create_distance_degree_header(id), range(1, max_person_count + 1)))
+        )
+        distance_degree_writer = csv.DictWriter(distance_degree_out, fieldnames=distance_degree_header)
+        distance_degree_writer.writeheader()
+        # 前のフレームがないので空行を書き込む
+        distance_degree_writer.writerow({"frame_num": get_frame_num("1")})
 
-            position_dict = {"frame_num": filename.replace(".json", "").replace("frame_", "")}
+        for window in sliding_window(files, 2):
+            (filename, next_filename) = cast(tuple[str, str], window)
 
-            for person in current_list:
-                warped_keypoints = warp_keypoints(person.keypoints)
+            with open(os.path.join(KEYPOINT_JSON_PATH, filename)) as current_file, open(
+                os.path.join(KEYPOINT_JSON_PATH, next_filename)
+            ) as next_file:
+                current_list: list[Person] = json.load(current_file, object_hook=as_person)
+                current_person_dict = {person.person_id: person for person in current_list}
 
-                # 位置座標
-                person_position = warped_keypoints[KeypointEnum.LEFT_ANKLE]
+                next_list: list[Person] = json.load(next_file, object_hook=as_person)
+                next_person_dict = {person.person_id: person for person in next_list}
 
-                if person_position.xy[0] == 0 and person_position.xy[1] == 0:
-                    continue
+            # 準備
+            current_frame_num = get_frame_num(filename)
+            next_frame_num = get_frame_num(next_filename)
+            position_dict = {"frame_num": current_frame_num}
+            distance_degree_dict = {"frame_num": next_frame_num}
 
-                position_header = create_position_header(person.person_id)
-                position_dict[position_header[0]] = person_position.xy[0]
-                position_dict[position_header[1]] = person_position.xy[1]
+            for person_id in range(1, max_person_count + 1):
+                if current_person_dict.get(person_id) is not None:
+                    current_warped_keypoints = warp_keypoints(current_person_dict[person_id].keypoints)
+                    current_middle_hip = Midpoint(
+                        current_warped_keypoints[KeypointEnum.LEFT_HIP],
+                        current_warped_keypoints[KeypointEnum.RIGHT_HIP],
+                    )
 
+                    # 位置座標
+                    current_person_position = current_warped_keypoints[KeypointEnum.LEFT_ANKLE]
+                    if current_person_position.xy[0] == 0 and current_person_position.xy[1] == 0:
+                        continue
+
+                    position_person_header = create_position_header(person_id)
+                    position_dict[position_person_header[0]] = current_person_position.xy[0]
+                    position_dict[position_person_header[1]] = current_person_position.xy[1]
+
+                    if next_person_dict.get(person_id) is not None:
+                        next_warped_keypoints = warp_keypoints(next_person_dict[person_id].keypoints)
+                        next_middle_hip = Midpoint(
+                            next_warped_keypoints[KeypointEnum.LEFT_HIP], next_warped_keypoints[KeypointEnum.RIGHT_HIP]
+                        )
+
+                        # 距離・角度
+                        distance = length(current_middle_hip.xy, next_middle_hip.xy)
+                        degree = get_body_orientation(
+                            current_middle_hip, next_middle_hip, current_warped_keypoints[KeypointEnum.LEFT_HIP]
+                        )
+
+                        distance_degree_person_header = create_distance_degree_header(person_id)
+                        distance_degree_dict[distance_degree_person_header[0]] = str(distance)
+                        distance_degree_dict[distance_degree_person_header[1]] = str(degree)
+
+            # 書き込み
             position_writer.writerow(position_dict)
+            distance_degree_writer.writerow(distance_degree_dict)
 
 
 if __name__ == "__main__":
--- src/usecase.py
+++ src/usecase.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 
 import numpy as np
-from calc import warp
+from calc import angle, to_degree, warp
 from dataclass import Keypoint
 from keypoint import KeypointEnum
 
@@ -28,3 +28,9 @@ class Midpoint:
 
 def warp_keypoints(keypoints: dict[KeypointEnum, Keypoint]) -> dict[KeypointEnum, WarpedKeypoint]:
     return {key: WarpedKeypoint(value) for (key, value) in keypoints.items()}
+
+
+def get_body_orientation(
+    current_middle_hip: Midpoint, next_middle_hip: Midpoint, current_left_hip: WarpedKeypoint
+) -> np.float64:
+    return 90.0 - to_degree(angle(current_middle_hip.xy, next_middle_hip.xy, current_left_hip.xy))
