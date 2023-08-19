5a8551745524b53eaa0a341cee4d2ea9d29471ab
ninebolt6 <67471674+ninebolt6@users.noreply.github.com>
2023-08-20 00:29:15 +0900

    CSVWriterを抽象化

--- src/csv_writer.py
+++ src/csv_writer.py
@@ -3,9 +3,10 @@ from io import TextIOWrapper
 from itertools import chain
 from numpy import float64, ndarray
 from usecase import WarpedKeypoint
+from abc import ABCMeta, abstractmethod
 
 
-class PositionWriter:
+class CsvWriterBase(metaclass=ABCMeta):
     def __init__(self, file: TextIOWrapper, max_person_count: int):
         header = ["frame_num"]
         header.extend(
@@ -17,6 +18,27 @@ class PositionWriter:
         self.writer.writeheader()
         self.buf = {}
 
+    def append_raw_data(self, key: str, value):
+        self.buf[key] = value
+
+    def __clear_buf(self):
+        self.buf = {}
+
+    @abstractmethod
+    def generate_header(self, id: int) -> list[str]:
+        pass
+
+    @abstractmethod
+    def append(self):
+        pass
+
+    def writerow(self, frame_num: str):
+        self.append_raw_data("frame_num", frame_num)
+        self.writer.writerow(self.buf)
+        self.__clear_buf()
+
+
+class PositionWriter(CsvWriterBase):
     def generate_header(self, id: int) -> list[str]:
         return [f"id:{id} x", f"id:{id} y"]
 
@@ -24,62 +46,25 @@ class PositionWriter:
         assert position.xy is not None
 
         target_header = self.generate_header(person_id)
-        self.buf[target_header[0]] = position.xy[0]
-        self.buf[target_header[1]] = position.xy[1]
-
-    def writerow(self, frame_num: str):
-        self.buf["frame_num"] = frame_num
-        self.writer.writerow(self.buf)
-        self.buf = {}
+        self.append_raw_data(target_header[0], position.xy[0])
+        self.append_raw_data(target_header[1], position.xy[1])
 
 
-class DistanceDegreeWriter:
-    def __init__(self, file: TextIOWrapper, max_person_count: int):
-        header = ["frame_num"]
-        header.extend(
-            # flatten
-            chain.from_iterable(map(lambda id: self.generate_header(id), range(1, max_person_count + 1)))
-        )
-
-        self.writer = csv.DictWriter(file, fieldnames=header)
-        self.writer.writeheader()
-        self.buf = {}
-
+class DistanceDegreeWriter(CsvWriterBase):
     def generate_header(self, id: int) -> list[str]:
         return [f"id:{id} dist", f"id:{id} deg"]
 
     def append(self, person_id: int, distance: float64, degree: float64):
         target_header = self.generate_header(person_id)
-        self.buf[target_header[0]] = distance
-        self.buf[target_header[1]] = degree
-
-    def writerow(self, frame_num: str):
-        self.buf["frame_num"] = frame_num
-        self.writer.writerow(self.buf)
-        self.buf = {}
+        self.append_raw_data(target_header[0], distance)
+        self.append_raw_data(target_header[1], degree)
 
 
-class RelativePositionWriter:
-    def __init__(self, file: TextIOWrapper, max_person_count: int):
-        header = ["frame_num"]
-        header.extend(
-            # flatten
-            chain.from_iterable(map(lambda id: self.generate_header(id), range(1, max_person_count + 1)))
-        )
-
-        self.writer = csv.DictWriter(file, fieldnames=header)
-        self.writer.writeheader()
-        self.buf = {}
-
+class RelativePositionWriter(CsvWriterBase):
     def generate_header(self, id: int) -> list[str]:
         return [f"id:{id} x", f"id:{id} y"]
 
     def append(self, person_id: int, relative_position: ndarray):
         target_header = self.generate_header(person_id)
-        self.buf[target_header[0]] = relative_position[0]
-        self.buf[target_header[1]] = relative_position[1]
-
-    def writerow(self, frame_num: str):
-        self.buf["frame_num"] = frame_num
-        self.writer.writerow(self.buf)
-        self.buf = {}
+        self.append_raw_data(target_header[0], relative_position[0])
+        self.append_raw_data(target_header[1], relative_position[1])
